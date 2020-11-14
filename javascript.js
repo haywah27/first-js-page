cb791cddd6f1e9b5c7735f88ca010fce02ca1654
haywah27 <hayleywahlroos15@gmail.com>
2020-11-13 16:39:23 -0800

    first commit

---
+++ javascript.js
@@ -0,0 +1,40 @@
+
+
+//grow button
+button1.addEventListener("click", function(){
+    box.style.height = "250px";
+    box.style.width = "250px";
+});
+
+//blue button
+button2.addEventListener("click", function(){
+    box.style.background = "#0000CD";
+});
+
+//fade button
+button3.addEventListener("click", function(){
+    box.style.opacity = "0.5";
+});
+
+//shrink button
+button4.addEventListener("click", function(){
+    box.style.height = "100px";
+    box.style.width = "100px";
+});
+
+//reset button
+button5.addEventListener("click", function(){
+    box.style.height = "150px";
+    box.style.width = "150px";
+    box.style.background = "#ff8c69";
+    box.style.opacity = "1";
+});
+
+
+
+
+
+
+
+
+
